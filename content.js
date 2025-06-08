92072a24acec0fc6a23d5c11ab99ce3cf5d62450
MatteoLucerni <lucernimatteo.off@gmail.com>
2025-06-08 18:54:19 +0200

    feat: visual hider fn

--- content.js
+++ content.js
@@ -81,8 +81,66 @@ function hideWatched() {
     });
 }
 
+function extractNumber(str) {
+  const cleanedStr = str.replace(/[^\d.,]/g, '');
+  const matched = cleanedStr.match(/[\d.,]+/);
+
+  return matched ? matched[0] : '';
+}
+
+function parseToNumber(str) {
+  if (!str) return NaN;
+
+  const hasDot = str.includes('.');
+  const hasComma = str.includes(',');
+
+  if (hasDot && hasComma) {
+    if (str.lasyIndexOf('.') > str.lastIndexOf(',')) {
+      return parseFloat(str.replace(/,/g, ''));
+    } else {
+      return parseFloat(str.replace(/\./g, '').replace(',', '.'));
+    }
+  } else if (hasComma) {
+    return parseFloat(str.replace(',', '.'));
+  } else {
+    return parseFloat(str);
+  }
+}
+
+function hideUnderVisuals() {
+  const { hideVisualThreshold } = prefs;
+
+  document
+    .querySelectorAll('#metadata-line #separator ~ span.inline-metadata-item')
+    .forEach(span => {
+      const rawNumber = extractNumber(span.textContent);
+      const views = parseToNumber(rawNumber);
+
+      console.log(views);
+
+      if (isNaN(views) && views >= hideVisualThreshold) return;
+
+      let item = span;
+      while (
+        item &&
+        !item.matches(
+          'ytd-compact-video-renderer, ytd-rich-item-renderer, ytd-video-renderer'
+        )
+      ) {
+        item = item.parentElement;
+      }
+
+      if (item) item.style.display = 'none';
+    });
+}
+
 function startHiding() {
-  const { hideHomeEnabled, hideSearchEnabled, hideSubsEnabled, hideCorrEnabled } = prefs;
+  const {
+    hideHomeEnabled,
+    hideSearchEnabled,
+    hideSubsEnabled,
+    hideCorrEnabled,
+  } = prefs;
   const { pathname } = window.location;
 
   if (
